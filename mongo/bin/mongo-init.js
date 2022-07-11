4d739dca9755179c3a34e56f64c3a7b096bed46d
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-07-11 21:19:00 +0700

    fix: make upy as root user of mongo service

--- mongo/bin/mongo-init.js
+++ mongo/bin/mongo-init.js
@@ -1,13 +1,3 @@
-use admin
-db.createUser({
-  user: 'upy',
-  pwd: 'tf4CcEuN3dENXLgs',
-  roles: [{
-    role: 'readWrite',
-    db: 'upy'
-  }]
-})
-
 db.createCollection('logs', { capped: false })
 db.createCollection('forgot-tokens', { capped: false })
 db.createCollection('junks', { capped: false })
