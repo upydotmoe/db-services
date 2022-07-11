a1b6657043b780dc93bda3cfe98cf88ffb1dd91c
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-07-11 21:04:04 +0700

    fix mongo not creating user

--- mongo/bin/mongo-init.js
+++ mongo/bin/mongo-init.js
@@ -1,15 +1,12 @@
-db.createUser(
-  {
-    user: 'upy',
-    pwd: 'tf4CcEuN3dENXLgs',
-    roles: [
-      {
-        role: 'readWrite',
-        db: 'upy'
-      }
-    ]
-  }
-)
+db.createUser({
+  user: 'upy',
+  pwd: 'tf4CcEuN3dENXLgs',
+  roles: [{
+    role: 'readWrite',
+    db: 'upy'
+  }]
+})
+
 db.createCollection('logs', { capped: false })
 db.createCollection('forgot-tokens', { capped: false })
 db.createCollection('junks', { capped: false })
