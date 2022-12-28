06a1b53a2f8e680a19019baeef0f0ddc77885077
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-12-28 11:59:07 +0700

    downgrade mongo version to 5.0.14 because mongo shell has been removed on mongo:6.0

--- mongo/init.sh
+++ mongo/init.sh
@@ -1,1 +1,1 @@
-mongosh --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); db = db.getSiblingDB('admin'); db.createUser({ user: '$MONGO_USER', pwd: '$MONGO_PASS', roles: [{ role: 'readWrite', db: '$MONGO_DB' }] });"
+mongo --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); db = db.getSiblingDB('admin'); db.createUser({ user: '$MONGO_USER', pwd: '$MONGO_PASS', roles: [{ role: 'readWrite', db: '$MONGO_DB' }] });"
